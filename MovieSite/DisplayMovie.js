4cf703a9a4743a87e31ab2d46f16496c86eb7f66
Sigurður Detlef <siggidetjons6@gmail.com>
2021-12-06 20:13:05 +0000

    Basics

---
+++ MovieSite/DisplayMovie.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const DisplayMovie = (props) => {
+    return(
+        <View>
+
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+export default DisplayMovie;
