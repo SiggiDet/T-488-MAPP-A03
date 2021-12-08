f4639cd9ac3d3fbeae242b912c72aaa7c7476d3d
Sigurður Detlef <siggidetjons6@gmail.com>
2021-12-08 15:10:17 +0000

    Implemented that the Movies are displayed in descending order

--- UpcomingMoviesFolder/DisplayUpcomingMovies.js
+++ UpcomingMoviesFolder/DisplayUpcomingMovies.js
@@ -7,12 +7,12 @@ import UpcomingMovieSite from './UpcomingMovieSite';
 
 
 const customData = require('../DummyData/UpcomingMovies.json');
-const UpcomingMovies = customData.UpcomingMovies;
+const UpcomingMovies = customData.UpcomingMovies.sort(function (one, another) {return one.Release.localeCompare(another.Release);});
 
 const  DisplayUpcomingMovies= (props) => {
     return (
         <ScrollView delaysContentTouches = {true} >
-            {UpcomingMovies.map(
+            {UpcomingMovies.reverse().map(
                 UpcomingMovies => {
                     return(
                         <UpcomingMovieSite Movie = {UpcomingMovies}/>
