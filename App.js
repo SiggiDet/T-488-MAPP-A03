02d3fee9c0b699a27b20801d292640c5d3875465
KypsLloyd <70693651+KypsLloyd@users.noreply.github.com>
2021-12-10 10:41:49 +0000

    Update App.js

--- App.js
+++ App.js
@@ -17,13 +17,13 @@ export default function App() {
 
     /*
     <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" /> */}
-      {/* <Movies/> */}
-      {/* <DisplayUpcomingMovies/> */}
-      {/* <CinemaList/> */}
-      {/* <CinemaDetail/> */}
-      {/* <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Movies/>
+      <DisplayUpcomingMovies/>
+      <CinemaList/>
+      <CinemaDetail/>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
 
